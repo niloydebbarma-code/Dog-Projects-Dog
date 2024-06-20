e4295f746801ce563e95e68d638075f5cdeb9fa1
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-21 03:48:57 +0600

    Update adjust_results4_isadog.py

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -1,30 +1,22 @@
-def calculates_results_stats(results_dic):
-    results_stats = dict()
-
-    results_stats['n_dogs_img'] = 0
-    results_stats['n_notdogs_img'] = 0
-    results_stats['n_correct_dogs'] = 0
-    results_stats['n_correct_notdogs'] = 0
-    results_stats['n_correct_breed'] = 0
-    results_stats['n_images'] = len(results_dic)
-
+def adjust_results4_isadog(results_dic, dogfile):
+    dognames = dict()
+    
+    with open(dogfile) as f:
+        line = f.readline()
+        while line != "":
+            line = line.rstrip()
+            if line not in dognames:
+                dognames[line] = 1
+            line = f.readline()
+    
     for key in results_dic:
-        if results_dic[key][3] == 1:
-            results_stats['n_dogs_img'] += 1
-
-            if results_dic[key][4] == 1:
-                results_stats['n_correct_dogs'] += 1
-
-            if results_dic[key][2] == 1:
-                results_stats['n_correct_breed'] += 1
+        if results_dic[key][0] in dognames:
+            if results_dic[key][1] in dognames:
+                results_dic[key].extend((1, 1))
+            else:
+                results_dic[key].extend((1, 0))
         else:
-            results_stats['n_notdogs_img'] += 1
-
-            if results_dic[key][4] == 0:
-                results_stats['n_correct_notdogs'] += 1
-
-    results_stats['pct_correct_dogs'] = (results_stats['n_correct_dogs'] / results_stats['n_dogs_img']) * 100.0
-    results_stats['pct_correct_breed'] = (results_stats['n_correct_breed'] / results_stats['n_dogs_img']) * 100.0
-    results_stats['pct_correct_notdogs'] = (results_stats['n_correct_notdogs'] / results_stats['n_notdogs_img']) * 100.0
-
-    return results_stats
+            if results_dic[key][1] in dognames:
+                results_dic[key].extend((0, 1))
+            else:
+                results_dic[key].extend((0, 0))
