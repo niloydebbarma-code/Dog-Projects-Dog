36ee41b7bc78921a8ce7a99e2dda38b226309b5f
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-22 19:35:22 +0600

    Update get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -1,8 +1,9 @@
 import argparse
 
 def get_input_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Classify pet images using a CNN model.')
     parser.add_argument('--dir', type=str, default='pet_images/', help='Path to the folder of pet images')
-    parser.add_argument('--arch', type=str, default='vgg', help='CNN model architecture to use')
+    parser.add_argument('--arch', type=str, default='vgg', choices=['resnet', 'alexnet', 'vgg'], help='CNN model architecture (choices: resnet, alexnet, vgg)')
     parser.add_argument('--dogfile', type=str, default='dognames.txt', help='Text file with dog names')
-    return parser.parse_args()
+    args = parser.parse_args()
+    return args
