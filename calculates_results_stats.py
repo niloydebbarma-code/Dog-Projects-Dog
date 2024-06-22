05861cac130c4f3d633f374c2d4b8f8bd16ce052
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-22 19:22:54 +0600

    Update calculates_results_stats.py

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -13,28 +13,26 @@ def calculates_results_stats(results_dic):
         'pct_correct_notdogs': 0.0
     }
     
-    for key in results_dic:
-        pet_label, classifier_label, match, is_dog, classifier_is_dog = results_dic[key]
-        
+    for key, (pet_label, classifier_label, match, is_dog, classifier_is_dog) in results_dic.items():
         results_stats_dic['n_images'] += 1
         
-        if is_dog == 1:
+        if is_dog:
             results_stats_dic['n_dogs_img'] += 1
-            if classifier_is_dog == 1:
+            if classifier_is_dog:
                 results_stats_dic['n_correct_dogs'] += 1
-                if match == 1:
+                if match:
                     results_stats_dic['n_correct_breed'] += 1
         else:
             results_stats_dic['n_notdogs_img'] += 1
-            if classifier_is_dog == 0 and match == 0:
+            if not classifier_is_dog and not match:
                 results_stats_dic['n_correct_notdogs'] += 1
         
-        if match == 1:
+        if match:
             results_stats_dic['n_match'] += 1
-            
+    
     if results_stats_dic['n_images'] > 0:
         results_stats_dic['pct_match'] = (results_stats_dic['n_match'] / results_stats_dic['n_images']) * 100.0
-        
+    
     if results_stats_dic['n_dogs_img'] > 0:
         results_stats_dic['pct_correct_dogs'] = (results_stats_dic['n_correct_dogs'] / results_stats_dic['n_dogs_img']) * 100.0
         results_stats_dic['pct_correct_breed'] = (results_stats_dic['n_correct_breed'] / results_stats_dic['n_dogs_img']) * 100.0
