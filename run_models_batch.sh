39284d6b325a8cd77702c1debb20ff011f5e4217
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-22 17:43:25 +0600

    Update run_models_batch.sh

--- run_models_batch.sh
+++ run_models_batch.sh
@@ -1,6 +1,4 @@
-### run_models_batch.sh
-
-```bash
-python check_images.py --dir pet_images/ --arch resnet --dogfile dognames.txt > resnet_pet-images.txt
-python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
-python check_images.py --dir pet_images/ --arch vgg --dogfile dognames.txt > vgg_pet-images.txt
+#!/bin/bash
+python check_images.py --dir uploaded_images --arch resnet --dogfile dognames.txt
+python check_images.py --dir uploaded_images --arch alexnet --dogfile dognames.txt
+python check_images.py --dir uploaded_images --arch vgg --dog
