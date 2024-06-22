3ad1e18740d02681b7a6dbaf75b984555693fd9f
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-22 17:38:17 +0600

    Update print_results.py

--- print_results.py
+++ print_results.py
@@ -1,29 +1,21 @@
-def print_results(results_dic, results_stats_dic, model, 
-                  print_incorrect_dogs = False, print_incorrect_breed = False):
-    print("\n\n*** Results Summary for CNN Model Architecture", model.upper(), "***")
-    print("%20s: %3d" % ('N Images', results_stats_dic['n_images']))
-    print("%20s: %3d" % ('N Dog Images', results_stats_dic['n_dogs_img']))
-    print("%20s: %3d" % ('N Not-Dog Images', results_stats_dic['n_notdogs_img']))
+def print_results(results_dic, results_stats, model, print_incorrect_dogs=False, print_incorrect_breed=False):
+    print(f"Results for CNN Model Architecture {model}\n")
+    print(f"N Images: {results_stats['n_images']}")
+    print(f"N Dog Images: {results_stats['n_dogs_img']}")
+    print(f"N Not-Dog Images: {results_stats['n_notdogs_img']}")
+    print(f"Pct Match: {results_stats['pct_match']:.2f}%")
+    print(f"Pct Correct Dogs: {results_stats['pct_correct_dogs']:.2f}%")
+    print(f"Pct Correct Breed: {results_stats['pct_correct_breed']:.2f}%")
+    print(f"Pct Correct Not-a-Dog: {results_stats['pct_correct_notdogs']:.2f}%")
 
-    for key in results_stats_dic:
-        if key[0] == 'p':
-            print("%20s: %5.1f" % (key, results_stats_dic[key]))
-
-    if (print_incorrect_dogs and 
-        ( (results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs']) 
-          != results_stats_dic['n_images'] ) 
-       ):
-        print("\nINCORRECT Dog/NOT Dog Assignments:")
-
+    if print_incorrect_dogs:
+        print("\nIncorrect Dog/Not Dog Assignments:")
         for key in results_dic:
-            if ( sum(results_dic[key][3:]) == 1 ):
-                print("Real: %-26s   Classifier: %-30s" % (results_dic[key][0], results_dic[key][1]))
-
-    if (print_incorrect_breed and 
-        ( results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed'] ) 
-       ):
-        print("\nINCORRECT Dog Breed Assignment:")
-
+            if sum(results_dic[key][3:]) == 1:
+                print(f"Real: {results_dic[key][1]}  Classifier: {results_dic[key][2]}")
+    
+    if print_incorrect_breed:
+        print("\nIncorrect Dog Breed Assignments:")
         for key in results_dic:
-            if ( sum(results_dic[key][3:]) == 2 and results_dic[key][2] == 0 ):
-                print("Real: %-26s   Classifier: %-30s" % (results_dic[key][0], results_dic[key][1]))
+            if sum(results_dic[key][3:]) == 2 and results_dic[key][3] == 1:
+                print(f"Real: {results_dic[key][1]}  Classifier: {results_dic[key][2]}")
