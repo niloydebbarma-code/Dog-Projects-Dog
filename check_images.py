8efbdc12e1e6415778670afdc9cbf08e60d41c41
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-22 17:37:32 +0600

    Update check_images.py

--- check_images.py
+++ check_images.py
@@ -1,5 +1,3 @@
-import argparse
-from time import time, sleep
 from get_input_args import get_input_args
 from get_pet_labels import get_pet_labels
 from classify_images import classify_images
@@ -8,35 +6,23 @@ from calculates_results_stats import calculates_results_stats
 from print_results import print_results
 
 def main():
-    # Measure total program runtime by collecting start time
-    start_time = time()
-
-    # Get command line arguments
+    # Retrieve input arguments
     in_arg = get_input_args()
-
-    # Get pet image labels
+    
+    # Get pet labels
     results = get_pet_labels(in_arg.dir)
-
-    # Get classification results
+    
+    # Classify images
     classify_images(in_arg.dir, results, in_arg.arch)
-
-    # Adjust results for is-a-dog/not-a-dog classification
+    
+    # Adjust results for is a dog
     adjust_results4_isadog(results, in_arg.dogfile)
-
+    
     # Calculate results statistics
     results_stats = calculates_results_stats(results)
-
+    
     # Print results
     print_results(results, results_stats, in_arg.arch, True, True)
-
-    # Measure total program runtime by collecting end time
-    end_time = time()
-
-    # Print total time to run
-    tot_time = end_time - start_time
-    print("\n** Total Elapsed Runtime:", str(int(tot_time / 3600)) + ":" + 
-          str(int((tot_time % 3600) / 60)) + ":" + 
-          str(int((tot_time % 3600) % 60)) )
 
 if __name__ == "__main__":
     main()
