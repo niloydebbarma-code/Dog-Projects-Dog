e862570bc73db2ebcf39b01c636bd1593a393c86
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-22 20:01:37 +0600

    Update run_models_batch.sh

--- run_models_batch.sh
+++ run_models_batch.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-python check_images.py --dir uploaded_images --arch resnet --dogfile dognames.txt
-python check_images.py --dir uploaded_images --arch alexnet --dogfile dognames.txt
-python check_images.py --dir uploaded_images --arch vgg --dog
+
+# Define variables
+dir="pet_images/"
+arch=("resnet" "alexnet" "vgg")
+dogfile="dognames.txt"
+log_dir="logs"
+
+# Create log directory if it doesn't exist
+mkdir -p "$log_dir"
+
+# Loop through each architecture and run check_images.py
+for model in "${arch[@]}"
+do
+    python check_images.py --dir "$dir" --arch "$model" --dogfile "$dogfile" > "$log_dir/${model}_pet-images.txt" 2>&1
+done
