076775bc4a9f159e96a26a15854799020e8aef3f
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-22 20:02:48 +0600

    Update run_models_batch_uploaded.sh

--- run_models_batch_uploaded.sh
+++ run_models_batch_uploaded.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
-python check_images.py --dir uploaded_images --arch resnet --dogfile dognames.txt
-python check_images.py --dir uploaded_images --arch alexnet --dogfile dognames.txt
-python check_images.py --dir uploaded_images --arch vgg --dog
+#!/bin/sh
+
+# Function to run check_images.py with specified architecture and output file
+run_model() {
+    python check_images.py --dir uploaded_images/ --arch $1 --dogfile dognames.txt > $2
+}
+
+# Use case statement to determine which model to run
+case $1 in
+    resnet)
+        run_model "resnet" "resnet_uploaded-images.txt"
+        ;;
+    alexnet)
+        run_model "alexnet" "alexnet_uploaded-images.txt"
+        ;;
+    vgg)
+        run_model "vgg" "vgg_uploaded-images.txt"
+        ;;
+    *)
+        echo "Invalid model specified. Usage: $0 {resnet|alexnet|vgg}"
+        exit 1
+        ;;
+esac
