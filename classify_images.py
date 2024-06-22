a0d6489cd7192e0a8d338494460eab41ebd86acf
Niloy Deb Barma <146704168+niloydebbarma-code@users.noreply.github.com>
2024-06-22 19:32:50 +0600

    Update classify_images.py

--- classify_images.py
+++ classify_images.py
@@ -1,6 +1,8 @@
 from classifier import classifier
 
 def classify_images(images_dir, results_dic, model):
-    for key in results_dic:
-        model_label = classifier(images_dir + key, model).lower().strip()
-        results_dic[key].extend([model_label, 1 if model_label in results_dic[key][0] else 0])
+    results_dic.update({
+        image_file: [details[0], classifier(images_dir + image_file, model).lower().strip(), 1 if details[0] in classifier(images_dir + image_file, model).lower().strip() else 0]
+        for image_file, details in results_dic.items()
+    })
+
